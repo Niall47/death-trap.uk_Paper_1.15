db71da705d03c426d7ce42e4501242c803795c76
Niall James <niall.james@dvla.gov.uk>
2019-12-14 17:59:53 +0000

    Notify other players if someone is trying to sleep

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 Runs on Paper MC 1.15
 
 - Removes books over 10 pages
+- Notifies other players when someone is trying to sleep
 
 This is primarily a testing ground for me to play with OOP.
--- src/main/java/uk/deathtrap/plugins/deathtrap/MyPLayerListener.java
+++ src/main/java/uk/deathtrap/plugins/deathtrap/MyPLayerListener.java
@@ -2,28 +2,19 @@ package uk.deathtrap.plugins.deathtrap;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockEvent;
-import org.bukkit.event.player.PlayerBucketEmptyEvent;
+import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerEditBookEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public final class MyPLayerListener implements Listener {
     public MyPLayerListener(DeathTrap plugin) {
-        Bukkit.getServer().getConsoleSender().sendRawMessage("My Player Listener starting up");
+        Bukkit.getServer().getConsoleSender().sendRawMessage("Death-Trap.uk starting up");
     }
-    @EventHandler
-    public void onConnect(PlayerLoginEvent event) {
-        Player player = event.getPlayer();
-    }
-
 
     @EventHandler
     public void antiBookBan(PlayerEditBookEvent event){
@@ -34,7 +25,20 @@ public final class MyPLayerListener implements Listener {
             int slot = event.getSlot();
             ItemStack book = player.getInventory().getItem(slot);
             book.setAmount(0);
+
             player.sendMessage(ChatColor.GOLD + "Nobody wants to read your " + pages + " page novel dude");
+            ItemStack item = new ItemStack(Material.WRITABLE_BOOK, 1);
+            player.getInventory().setItem(slot, item);
+
+        }
+    }
+    @EventHandler
+    public void goToBed(PlayerBedEnterEvent event) {
+        int numberOfPlayers = Bukkit.getOnlinePlayers().size();
+        String name = event.getPlayer().getDisplayName();
+        if (numberOfPlayers > 0) {
+            Bukkit.getConsoleSender().sendMessage(name + " is trying to sleep");
+            Bukkit.broadcastMessage(name + " is trying to sleep, consider sleeping or disconnecting");
 
         }
     }
@@ -43,13 +47,9 @@ public final class MyPLayerListener implements Listener {
     public void onLogin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         Bukkit.getServer().getConsoleSender().sendRawMessage(player.getDisplayName() + " has joined the server!");
-        player.sendMessage(ChatColor.GOLD + "Welcome to the server");
-        if (player.hasPlayedBefore()) {
-            player.sendMessage(ChatColor.GOLD + "You are not new");
-            player.setGameMode(GameMode.SURVIVAL);
-        }else{
-            player.sendMessage(ChatColor.GOLD + "Welcome to Death-Trap.uk +" +
-                    " Enjoy your stay");
+        if (!player.hasPlayedBefore()) {
+            player.sendMessage(ChatColor.GOLD + "Welcome to Death-Trap.uk");
+            player.sendMessage(ChatColor.GOLD + "Visit Death-Trap.uk for voting rewards and updates");
         }
     }
     }
